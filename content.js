cabe088f32a0e34d47c3e280462f9caf1549ec4d
François <fxpar33@gmail.com>
2019-11-11 16:25:52 +0100

    détection langue navigateur

--- content.js
+++ content.js
@@ -3,6 +3,11 @@
   var délai = [1, 1, 1, 1, 1, 3, 2]; 
   var livraison = new Date();
   livraison.setDate(auj.getDate()+délai[auj.getDay()]);
+
+  //détecter le language du navigateur pour savoir comment imprimer la date: 20/09/2019 ou 09/20/2019
+  var language = window.navigator.userLanguage || window.navigator.language;
+
+//si le curseur est dans une zone de texte, alors on ajoute la date au début
 	if ((document.activeElement.type == "text")||(document.activeElement.type == "textarea")){
-  document.activeElement.value = livraison.toLocaleDateString('fr-FR')+document.activeElement.value;
+  document.activeElement.value = livraison.toLocaleDateString('language')+document.activeElement.value;
   }
