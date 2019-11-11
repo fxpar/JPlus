32c756e5c7f5ba60b69801a84963b6de607461e3
François <fxpar33@gmail.com>
2019-11-11 15:22:13 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
   var auj = new Date();
   //délai pour chaque jour, commence à dimanche
-  var délai = [1, 1, 1, 1, 1,3, 2]; 
+  var délai = [1, 1, 1, 1, 1, 3, 2]; 
   var livraison = new Date();
   livraison.setDate(auj.getDate()+délai[auj.getDay()]);
 	if ((document.activeElement.type == "text")||(document.activeElement.type == "textarea")){
